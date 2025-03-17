817029fe9875e8e6fd15535c958cb584172c2b86
Karsten Larson <karsten.larson.1@gmail.com>
2025-03-17 11:51:16 -0500

    Day one changes

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   imports: [],
   templateUrl: './child.component.html',
-  styleUrl: './child.component.css'
+  styleUrl: './child.component.css',
 })
 export class ChildComponent {
-
+  @Input() inputText: string = '';
+  @Output() outputValue: EventEmitter<string> = new EventEmitter<string>();
 }
